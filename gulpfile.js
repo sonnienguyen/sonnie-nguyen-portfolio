1a5f0914590ccd082fce0a607971aa877b9c5cb6
Sonnie Nguyen <info@sonnienguyen.com>
2018-03-24 20:02:16 +1100

    Remove gulp-shell and replace with child_process from gulp.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,18 @@
-var gulp = require('gulp');
-var shell = require('gulp-shell');
-var browserSync = require('browser-sync').create();
+var gulp = require("gulp");
+var child = require('child_process');
+var browserSync = require("browser-sync").create();
 
 // Task for building blog when something changed:
-// gulp.task('build', shell.task(['bundle exec jekyll build --watch']));
-// Or if you don't use bundle:
-gulp.task('build', shell.task(['jekyll build --watch']));
+gulp.task("jekyll-rebuild", function() {
+  child.spawn('jekyll', ['build', '--watch', '--incremental'], {stdio: 'inherit'});
+  });
 
 // Task for serving blog with Browsersync
-gulp.task('serve', function () {
-    browserSync.init({server: {baseDir: '_site/'}});
-    // Reloads page when some of the already built files changed:
-    gulp.watch('_site/**/*.*').on('change', browserSync.reload);
+gulp.task("serve", function() {
+  browserSync.init({ server: { baseDir: "_site/" } });
+  // Reloads page when some of the already built files changed:
+  gulp.watch("_site/**/*.*").on("change", browserSync.reload);
 });
 
-gulp.task('default', ['build', 'serve']);
+// Default task
+gulp.task("default", ["jekyll-rebuild", "serve"]);
