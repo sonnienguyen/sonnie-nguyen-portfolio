656eb615a0ae9a5aa47d15ae72b8def949ae86c8
Sonnie Nguyen <info@sonnienguyen.com>
2018-03-26 14:59:31 +1100

    Add sass and autoprefixer to gulpfile.js config

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,36 @@
-var gulp = require("gulp");
-var child = require('child_process');
-var browserSync = require("browser-sync").create();
+var gulp = require('gulp'),
+child = require('child_process'),
+browserSync = require('browser-sync').create(),
+sass = require('gulp-sass'),
+prefix = require('gulp-autoprefixer');
 
 // Task for building blog when something changed:
-gulp.task("jekyll-rebuild", function() {
+gulp.task('jekyll-rebuild', function() {
   child.spawn('jekyll', ['build', '--watch', '--incremental'], {stdio: 'inherit'});
   });
 
+// Compile files
+gulp.task('sass', function () {
+  return gulp.src('assets/sass/main.scss')
+  .pipe(sass({
+    outputStyle: 'compressed'
+  }))
+  .pipe(prefix())
+  .on('error', function(errorInfo) {
+    console.log(errorInfo.toString());
+    this.emit('end');
+  })
+  .pipe(gulp.dest('_site/assets/css'))
+  .pipe(gulp.dest('assets/css'));
+});
+
 // Task for serving blog with Browsersync
-gulp.task("serve", function() {
-  browserSync.init({ server: { baseDir: "_site/" } });
+gulp.task('watch', function() {
+  browserSync.init({ notify: false, server: { baseDir: '_site/' } });
   // Reloads page when some of the already built files changed:
-  gulp.watch("_site/**/*.*").on("change", browserSync.reload);
+  gulp.watch(['assets/sass/*.scss', 'assets/**/*.scss'], ['sass']);
+  gulp.watch('_site/**/*.*').on('change', browserSync.reload);
 });
 
 // Default task
-gulp.task("default", ["jekyll-rebuild", "serve"]);
+gulp.task('default', ['jekyll-rebuild', 'watch']);
