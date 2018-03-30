dd5df1dac042ede59fbc2bf30af284214009a556
Sonnie Nguyen <info@sonnienguyen.com>
2018-03-30 20:50:15 +1100

    Add gulp error handling and CSS auto-inject to gulpfile.js config

--- gulpfile.js
+++ gulpfile.js
@@ -5,33 +5,40 @@ sass = require('gulp-sass'),
 prefix = require('gulp-autoprefixer');
 
 // Task for building blog when something changed:
-gulp.task('jekyll-rebuild', function() {
-  child.spawn('jekyll', ['build', '--watch', '--incremental'], {stdio: 'inherit'});
-  });
-
-// Compile files
-gulp.task('sass', function () {
-  return gulp.src('_sass/main.scss')
-  .pipe(sass({
-    outputStyle: 'compressed',
-    includePaths: ['node_modules']
-  }))
-  .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
-  .on('error', function(errorInfo) {
-    console.log(errorInfo.toString());
-    this.emit('end');
-  })
-  .pipe(gulp.dest('_site/assets/css'))
-  .pipe(gulp.dest('assets/css'));
+gulp.task('jekyll-build', function() {
+  return child.spawn('jekyll', ['build', '--watch', '--incremental'], { stdio: 'inherit' })
 });
 
-// Task for serving blog with Browsersync
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('cssInject', ['sass'], function() {
+  return gulp.src('assets/css/main.css')
+  .pipe(browserSync.stream());
+});
+
+// Compile sass into CSS
+gulp.task('sass', function() {
+  return gulp
+    .src('_sass/main.scss')
+    .pipe(sass({
+        outputStyle: 'compressed',
+        includePaths: ['node_modules']
+      }).on('error', sass.logError)
+    )
+    .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+    .pipe(gulp.dest('_site/assets/css'))
+    .pipe(gulp.dest('assets/css'));
+});
+
+// Task for serving blog with browserSync + watching scss/html files
 gulp.task('watch', function() {
+  // Serve files from the root of this project
   browserSync.init({ notify: false, server: { baseDir: '_site/' } });
   // Reloads page when some of the already built files changed:
-  gulp.watch(['_sass/*.scss', '_sass/**/*.scss'], ['sass']);
+  gulp.watch(['_sass/*.scss', '_sass/**/*.scss'], function() {
+    gulp.start('cssInject');
+  });
   gulp.watch('_site/**/*.*').on('change', browserSync.reload);
 });
 
 // Default task
-gulp.task('default', ['jekyll-rebuild', 'watch']);
+gulp.task('default', ['jekyll-build', 'watch']);
