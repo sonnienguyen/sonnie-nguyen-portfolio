70c6b87f9cbed014aa81a3f26ee9402deff9a2be
Sonnie Nguyen <info@sonnienguyen.com>
2018-04-11 15:24:53 +1000

    Update gulpfile.js config

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,16 @@ browserSync = require('browser-sync').create(),
 sass = require('gulp-sass'),
 prefix = require('gulp-autoprefixer');
 
-// Task for building blog when something changed:
-gulp.task('jekyll-build', function() {
-  return child.spawn('jekyll', ['build', '--watch', '--incremental'], { stdio: 'inherit' })
+var jekyll = process.platform === 'win32' ? 'jekyll.bat' : 'jekyll';
+
+// Task for building blog when something changed
+gulp.task('jekyll-build', function(done) {
+  return child.spawn(jekyll, ['build', '--incremental'], { stdio: 'inherit' }).on('close', done);
+});
+
+// Rebuild Jekyll and reload browserSync
+gulp.task('jekyll-rebuild', ['jekyll-build'], function() {
+  browserSync.reload();
 });
 
 // Compile sass into CSS & auto-inject into browsers
@@ -21,7 +28,8 @@ gulp.task('sass', function() {
     .src('_sass/main.scss')
     .pipe(sass({
         outputStyle: 'expanded',
-        includePaths: ['node_modules']
+        includePaths: ['node_modules'],
+        onError: browserSync.notify
       }).on('error', sass.logError)
     )
     .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
@@ -33,12 +41,12 @@ gulp.task('sass', function() {
 gulp.task('watch', function() {
   // Serve files from the root of this project
   browserSync.init({ notify: false, server: { baseDir: '_site/' } });
-  // Reloads page when some of the already built files changed:
+  // Reloads page when some of the already built files changed
   gulp.watch(['_sass/*.scss', '_sass/**/*.scss'], function() {
     gulp.start('cssInject');
   });
-  gulp.watch('_site/**/*.*').on('change', browserSync.reload);
+  gulp.watch(['*.html', '_categories/*', '_includes/*.html', '_layouts/*.html', '_pages/*.html', '_posts/*', '_posts/**/*'], ['jekyll-rebuild']);
 });
 
 // Default task
-gulp.task('default', ['jekyll-build', 'watch']);
+gulp.task('default', ['watch']);
