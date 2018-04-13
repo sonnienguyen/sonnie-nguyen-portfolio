f92a576ae22ea6a2717d897e5d2f263b0e372941
Sonnie Nguyen <info@sonnienguyen.com>
2018-04-13 17:12:14 +1000

    Update package.json

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,19 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "assets/js"),
     filename: "app.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['es2015']
+          }
+        }
+      }
+    ]
   }
 }
