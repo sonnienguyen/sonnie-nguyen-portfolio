214d32e0672fc51a44773fbc3f0a7ca25cda57ab
Sonnie Nguyen <info@sonnienguyen.com>
2018-04-14 20:08:14 +1000

    Refactor TimeAgo.js codebase to use classes

--- src/js/modules/TimeAgo.js
+++ src/js/modules/TimeAgo.js
@@ -1,39 +1,44 @@
-function TimeAgo(date) {
-  function render(n, unit) {
-    return `${n} ${unit}${n === 1 ? '' : 's'} ago`;
+class TimeAgo {
+  constructor() {
+    this.dateTime = document.querySelectorAll('#js-posted-on');
+    this.postedAgo = document.querySelectorAll('#js-posted-ago');
+    this.date();
   }
 
-  const seconds = Math.floor((new Date() - date) / 1000);
+  date() {
+    for (let i = 0; i < this.dateTime.length; i++) {
+      this.postedAgo[i].innerHTML = this.convertToSeconds(Date.parse(this.dateTime[i].getAttribute('datetime')));
+    }
+  }
 
-  let interval = Math.floor(seconds / (60 * 60 * 24 * 30 * 365));
-  if (Math.floor(seconds / (60 * 60 * 24 * 30 * 365)) >= 1) {
-    return render(interval, 'year');
-  }
-  interval = Math.floor(seconds / (60 * 60 * 24 * 30));
-  if (interval >= 1) {
-    return render(interval, 'month');
-  }
-  interval = Math.floor(seconds / (60 * 60 * 24));
-  if (interval >= 1) {
-    return render(interval, 'day');
-  }
-  interval = Math.floor(seconds / (60 * 60));
-  if (interval >= 1) {
-    return render(interval, 'hour');
-  }
-  interval = Math.floor(seconds / 60);
-  if (interval >= 1) {
-    return render(interval, 'minute');
-  }
-  interval = Math.floor(seconds);
-  return render(interval, 'second');
-}
+  convertToSeconds(date) {
+    this.seconds = Math.floor((new Date() - date) / 1000);
+    this.interval = Math.floor(this.seconds / 31536000);
 
-const dateTime = document.querySelectorAll('#js-posted-on');
-const postedAgo = document.querySelectorAll('#js-posted-ago');
+    if (this.interval >= 1) {
+      return this.render(this.interval, 'year');
+    }
+    this.interval = Math.floor(this.seconds / 86400);
+    if (this.interval >= 1) {
+      return this.render(this.interval, 'day');
+    }
+    this.interval = Math.floor(this.seconds / 3600);
+    if (this.interval >= 1) {
+      return this.render(this.interval, 'hour');
+    }
+    this.interval = Math.floor(this.seconds / 60);
+    if (this.interval >= 1) {
+      return this.render(this.interval, 'minute');
+    }
+    this.interval = Math.floor(this.seconds);
+    return this.render(this.interval, 'second');
+  }
 
-for (let i = 0; i < dateTime.length; i++) {
-  postedAgo[i].innerHTML = TimeAgo(Date.parse(dateTime[i].getAttribute('datetime')));
+  render(n, unit) {
+    this.unit = unit;
+    this.n = n;
+    return `${this.n} ${this.unit}${this.n === 1 ? '' : 's'} ago`;
+  }
 }
 
 export default TimeAgo;
