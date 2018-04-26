9485461aae8046169f4f513d55dadf859a4c5048
Sonnie Nguyen <info@sonnienguyen.com>
2018-04-27 08:16:54 +1000

    Refactor TimeAgo.js code base

--- src/js/modules/TimeAgo.js
+++ src/js/modules/TimeAgo.js
@@ -1,43 +1,51 @@
-class TimeAgo {
-  constructor() {
-    this.dateTime = document.querySelectorAll('#js-posted-on');
-    this.postedAgo = document.querySelectorAll('#js-posted-ago');
-    this.date();
+function TimeAgo() {
+  const dateTime = document.querySelectorAll('#js-posted-on');
+  const postedAgo = document.querySelectorAll('#js-posted-ago');
+
+  function format(diff, divisor, unit, prev) {
+    const val = Math.floor(diff / divisor);
+    return val <= 1 ? prev : val + ' ' + unit + 's ago';
   }
 
-  date() {
-    for (let i = 0; i < this.dateTime.length; i++) {
-      this.postedAgo[i].innerHTML = this.convertToSeconds(Date.parse(this.dateTime[i].getAttribute('datetime')));
+  function ago(date) {
+    const diff = Math.abs(Date.now() - date.getTime());
+    const units = ago.units;
+
+    if (diff < 60000) {
+      // less than a minute
+      return 'just now';
+    }
+
+    for (let i = 0; i < units.length; i++) {
+      if (diff < units[i].max) {
+        return format(diff, units[i].value, units[i].name, units[i].prev);
+      }
     }
   }
 
-  convertToSeconds(date) {
-    this.seconds = Math.floor((new Date() - date) / 1000);
-    this.interval = Math.floor(this.seconds / 31536000);
+  ago.units = [
+    { // max: 46 minutes
+      max: 2760000, value: 60000, name: 'minute', prev: 'a minute ago',
+    },
+    { // max: 20 hours
+      max: 72000000, value: 3600000, name: 'hour', prev: 'an hour ago',
+    },
+    { // max: 6 days
+      max: 518400000, value: 86400000, name: 'day', prev: 'yesterday',
+    },
+    { // max: 28 days
+      max: 2419200000, value: 604800000, name: 'week', prev: 'last week',
+    },
+    { // max: 11 months
+      max: 28512000000, value: 2592000000, name: 'month', prev: 'last month',
+    },
+    {
+      max: Infinity, value: 31536000000, name: 'year', prev: 'last year',
+    },
+  ];
 
-    if (this.interval >= 1) {
-      return this.render(this.interval, 'year');
-    }
-    this.interval = Math.floor(this.seconds / 86400);
-    if (this.interval >= 1) {
-      return this.render(this.interval, 'day');
-    }
-    this.interval = Math.floor(this.seconds / 3600);
-    if (this.interval >= 1) {
-      return this.render(this.interval, 'hour');
-    }
-    this.interval = Math.floor(this.seconds / 60);
-    if (this.interval >= 1) {
-      return this.render(this.interval, 'minute');
-    }
-    this.interval = Math.floor(this.seconds);
-    return this.render(this.interval, 'second');
-  }
-
-  render(n, unit) {
-    this.unit = unit;
-    this.n = n;
-    return `${this.n} ${this.unit}${this.n === 1 ? '' : 's'} ago`;
+  for (let i = 0; i < dateTime.length; i++) {
+    postedAgo[i].innerHTML = ago(new Date(dateTime[i].getAttribute('datetime')));
   }
 }
 
