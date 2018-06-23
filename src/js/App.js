ffbf4a0c26b8ed24b0006cf2e36650f05dd58ff4
Sonnie Nguyen <info@sonnienguyen.com>
2018-06-23 10:45:54 +1000

    Add copyright and license comments at the top of the files

--- src/js/App.js
+++ src/js/App.js
@@ -1,3 +1,9 @@
+/*!
+ * Sonnie Nguyen's Portfolio Website
+ * Copyright 2017-2018 Sonnie Nguyen <info@sonnienguyen.com> | @sonnienguyen
+ * Licensed under MIT (https://github.com/sonnienguyen/sonnienguyen.github.io/blob/master/LICENSE)
+ */
+
 import FixedResponsiveNav from './vendors/FixedResponsiveNav';
 import TimeAgo from './modules/TimeAgo';
 
--- src/js/Vendor.js
+++ src/js/Vendor.js
@@ -1,3 +1,9 @@
+/*!
+ * Sonnie Nguyen's Portfolio Website
+ * Copyright 2017-2018 Sonnie Nguyen <info@sonnienguyen.com> | @sonnienguyen
+ * Licensed under MIT (https://github.com/sonnienguyen/sonnienguyen.github.io/blob/master/LICENSE)
+ */
+
 import ResponsiveNav from './vendors/ResponsiveNav.js';
 
 const responsiveNav = new ResponsiveNav(document, window, 0);
