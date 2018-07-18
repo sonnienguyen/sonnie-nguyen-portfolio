49adb2722f82b7a368e72daf65a3790e0c492fdc
Sonnie Nguyen <info@sonnienguyen.com>
2018-07-19 07:04:10 +1000

    refactor: add `s-ago` module to convert Date objects
    This is the smallest module to convert Date objects into human readable
    relative timestamps.

--- src/js/components/TimeAgo.js
+++ src/js/components/TimeAgo.js
@@ -1,52 +1,10 @@
+import ago from 's-ago';
+import { qsa } from './Helpers';
+
+const TimeAgo = (dateTime = qsa('#js-posted-on')) => {
+  for (let i = 0; i < dateTime.length; i++) {
+    qsa('#js-posted-ago')[i].innerHTML = ago(new Date(dateTime[i].getAttribute('datetime')));
+  }
+};
 
 export default TimeAgo;
-
-const units = [
-  { // max: 46 minutes
-    max: 2760000, value: 60000, name: 'minute', prev: 'a minute ago',
-  },
-  { // max: 20 hours
-    max: 72000000, value: 3600000, name: 'hour', prev: 'an hour ago',
-  },
-  { // max: 6 days
-    max: 518400000, value: 86400000, name: 'day', prev: 'yesterday',
-  },
-  { // max: 28 days
-    max: 2419200000, value: 604800000, name: 'week', prev: 'last week',
-  },
-  { // max: 11 months
-    max: 28512000000, value: 2592000000, name: 'month', prev: 'last month',
-  },
-  {
-    max: Infinity, value: 31536000000, name: 'year', prev: 'last year',
-  },
-];
-
-const dateTime = document.querySelectorAll('#js-posted-on');
-const postedAgo = document.querySelectorAll('#js-posted-ago');
-
-function format(diff, divisor, unit, prev) {
-  const val = Math.floor(diff / divisor);
-  return val <= 1 ? prev : val + ' ' + unit + 's ago';
-}
-
-function ago(date) {
-  const diff = Math.abs(Date.now() - date.getTime());
-
-  if (diff < 60000) {
-    // less than a minute
-    return 'just now';
-  }
-
-  for (let i = 0; i < units.length; i++) {
-    if (diff < units[i].max) {
-      return format(diff, units[i].value, units[i].name, units[i].prev);
-    }
-  }
-}
-
-function TimeAgo() {
-  for (let i = 0; i < dateTime.length; i++) {
-    postedAgo[i].innerHTML = ago(new Date(dateTime[i].getAttribute('datetime')));
-  }
-}
