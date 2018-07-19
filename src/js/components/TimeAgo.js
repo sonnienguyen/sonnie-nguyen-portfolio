6a1d33e0185c6caedb622b40b4e77de362defdc8
Sonnie Nguyen <info@sonnienguyen.com>
2018-07-20 06:59:52 +1000

    fix: element ID attribute must be unique
    Use class selector instead of element ID.

--- src/js/components/TimeAgo.js
+++ src/js/components/TimeAgo.js
@@ -1,9 +1,9 @@
 import ago from 's-ago';
 import { qsa } from './Helpers';
 
-const TimeAgo = (dateTime = qsa('#js-posted-on')) => {
+const TimeAgo = (dateTime = qsa('.js-posted-on')) => {
   for (let i = 0; i < dateTime.length; i++) {
-    qsa('#js-posted-ago')[i].innerHTML = ago(new Date(dateTime[i].getAttribute('datetime')));
+    qsa('.js-posted-ago')[i].innerHTML = ago(new Date(dateTime[i].getAttribute('datetime')));
   }
 };
 
