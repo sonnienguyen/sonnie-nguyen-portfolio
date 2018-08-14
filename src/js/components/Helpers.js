e6b186cad07bdd6aeb9dcbd01faca8c17ad6a380
Sonnie Nguyen <info@sonnienguyen.com>
2018-08-15 07:35:42 +1000

    docs(src): add comments to Helpers functions

--- src/js/components/Helpers.js
+++ src/js/components/Helpers.js
@@ -1,8 +1,22 @@
+/**
+ * Shortcut for `querySelector`.
+ *
+ * @param {string} selector
+ * @param {element} [scope] (optional)
+ * @returns {element}
+ */
 
-// Get elements by CSS selector:
 export function qs(selector, scope) {
   return (scope || document).querySelector(selector);
 }
+
+/**
+ * Shortcut for `querySelectorAll`.
+ *
+ * @param {string} selector
+ * @param {element} [scope] (optional)
+ * @returns {element}
+ */
 
 export function qsa(selector, scope) {
   return (scope || document).querySelectorAll(selector);
