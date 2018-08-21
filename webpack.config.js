b0099c42580cc9838fa27fd2501cd4223d6f8f51
Sonnie Nguyen <info@sonnienguyen.com>
2018-08-21 13:29:25 +1000

    build(webpack): add devtool option for debugging

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,7 @@ module.exports = {
     path: path.resolve(__dirname, 'docs/assets/js'),
     filename: '[name].js',
   },
+  devtool: 'cheap-module-eval-source-map',
   module: {
     rules: [
       {
