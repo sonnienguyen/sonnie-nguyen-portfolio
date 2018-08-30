8e584936b168c323ae7a03aff6d8a072d1d81e60
Sonnie Nguyen <info@sonnienguyen.com>
2018-08-30 21:19:29 +1000

    build: update prismjs to include more languages

--- src/js/app.js
+++ src/js/app.js
@@ -4,8 +4,10 @@
  * Licensed under MIT (https://github.com/sonnienguyen/sonnienguyen.github.io/blob/master/LICENSE)
  */
 
+import Prism from 'prismjs';
 import fixedResponsiveNav from './vendor/fixedResponsiveNav';
 import { humanizeDate } from './components/helpers';
 
+Prism.highlightAll();
 const displayFixedResponsiveNav = fixedResponsiveNav();
 const postDate = humanizeDate();
