c10a97bc3a1a81a39d041347d3fd53b4302e6e04
Sonnie Nguyen <info@sonnienguyen.com>
2018-09-03 06:09:44 +1000

    build: migrate from gulp to webpack 4

--- webpack.config.js
+++ webpack.config.js
@@ -1,54 +1,105 @@
 const path = require('path');
 const child = require('child_process');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
-module.exports = {
-  context: path.resolve(__dirname, 'src'),
-  entry: {
-    app: './js/app.js',
-  },
-  output: {
-    path: path.resolve(__dirname, 'docs/assets/js'),
-    filename: '[name].js',
-  },
-  // for more information, see https://webpack.js.org/configuration/devtool/#devtool
-  devtool: 'cheap-module-eval-source-map',
-  module: {
-    rules: [
+module.exports = (env, options) => {
+  const isProductionMode = options.mode === 'production';
+
+  // copies entire directories to the build directory
+  const copyFiles = [
+    { from: './fonts', to: '../css/fonts' },
+    { from: './images', to: '../images' },
+  ];
+
+  // remove/clean build folder(s) before webpack builds
+  const cleanFolders = {
+    path: isProductionMode
+      ? ['_site', 'docs/assets']
+      : ['_site'],
+  };
+
+  const webpackConfig = {
+    context: path.resolve(__dirname, 'src'),
+    entry: {
+      app: ['./js/app.js', './scss/main.scss'],
+    },
+    output: {
+      path: path.resolve(__dirname, 'docs/assets/js'),
+      filename: '[name].js',
+    },
+    // for more information, see https://webpack.js.org/configuration/devtool/#devtool
+    devtool: isProductionMode
+      ? 'source-map'
+      : 'cheap-module-eval-source-map',
+    module: {
+      rules: [
+        {
+          /* JS loaders config */
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+          },
+        },
+        {
+          /* CSS loaders config */
+          test: /\.s[ac]ss$/,
+          use: [
+            // `mini-css-extract-plugin` plugin should be used only on
+            // `production` builds without `style-loader` in the loaders chain
+            isProductionMode ? MiniCssExtractPlugin.loader : 'style-loader',
+            'css-loader',
+            'postcss-loader',
+            'sass-loader',
+          ],
+        },
+      ],
+    },
+    plugins: [
       {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
+        // run shell commands after webpack builds
+        apply: compiler => {
+          compiler.hooks.afterEmit.tap('AfterEmitPlugin', () => {
+            child.exec(
+              'bundle exec jekyll build --config docs/_config.yml',
+              (err, stdout, stderr) => {
+                if (stdout) process.stdout.write(stdout);
+                if (stderr) process.stderr.write(stderr);
+              },
+            );
+          });
         },
       },
-    ],
-  },
-  plugins: [
-    {
-      // run shell commands after webpack builds
-      apply: compiler => {
-        compiler.hooks.afterEmit.tap('AfterEmitPlugin', () => {
-          child.exec(
-            'bundle exec jekyll build --config ./docs/_config.yml',
-            (err, stdout, stderr) => {
-              if (stdout) process.stdout.write(stdout);
-              if (stderr) process.stderr.write(stderr);
-            },
-          );
-        });
-      },
-    },
-    new CleanWebpackPlugin(['_site']),
-  ],
-  // tell webpack to minimize the bundle
-  optimization: {
-    minimizer: [
-      new UglifyJsPlugin({
-        cache: true,
-        parallel: true,
+      new MiniCssExtractPlugin({
+        filename: '../css/main.css',
       }),
+      new CopyWebpackPlugin(copyFiles),
+      new CleanWebpackPlugin(cleanFolders.path),
     ],
-  },
+    stats: {
+      colors: true,
+      timings: true,
+      version: false,
+      warnings: true,
+    },
+    // tell webpack to minimize the bundle
+    optimization: {
+      minimize: isProductionMode,
+      minimizer: [
+        new UglifyJsPlugin({
+          cache: true,
+          parallel: true,
+        }),
+        new OptimizeCSSAssetsPlugin({
+          cssProcessorOptions: { discardComments: { removeAll: true } },
+        }),
+      ],
+    },
+  };
+
+  return webpackConfig;
 };
